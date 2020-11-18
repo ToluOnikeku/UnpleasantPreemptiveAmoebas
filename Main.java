df1a69da80fd93a2e163ff2797b94de68e6fda61
ToluOnikeku <toluwa.onikeku@patelhs.org>
2020-11-18 00:51:37 +0000

    Just added a little bit

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 class Main {
   public static void main(String[] args) {
-   //Create an array of 10 int values (must have both even and odd numbers)
-   int [1,2,3,4,5,6,7,8,9,10]
+   //Create an array of 15 elements (must have both even and odd numbers)
+   int [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    
